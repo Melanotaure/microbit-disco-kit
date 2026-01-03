4ecb3750315933701db8ef8b67305314c27c3701
unknown <luc.gaborit@gmail.com>
2026-01-03 17:35:26 +0100

    added sending data to UART to plot the temp data with teleplot (vscode)

--- lcd1602_temp/src/main.rs
+++ lcd1602_temp/src/main.rs
@@ -1,19 +1,23 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+use heapless::String;
+
 use defmt::info;
-use defmt_rtt as _;
+use defmt_rtt as _; // global logger
 use panic_halt as _;
 
 use embassy_executor::Spawner;
 use embassy_futures::join::join;
 use embassy_nrf::{
     bind_interrupts,
-    peripherals::TWISPI0,
+    peripherals::{self, TWISPI0},
     temp::Temp,
     twim::{self, Twim},
+    uarte,
 };
-use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, channel::Channel, signal::Signal};
 use embassy_time::{Delay, Timer};
 use hd44780_controller::{
     controller::{
@@ -25,16 +29,24 @@ use hd44780_controller::{
 };
 
 static SIGNAL: Signal<ThreadModeRawMutex, u16> = Signal::new();
+static UART_CHANNEL: Channel<ThreadModeRawMutex, String<64>, 8> = Channel::new();
 
 bind_interrupts!( struct Irqs {
     TWISPI0 => twim::InterruptHandler<TWISPI0>;
     TEMP => embassy_nrf::temp::InterruptHandler;
+    UARTE0 => uarte::InterruptHandler<peripherals::UARTE0>;
 });
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     info!("Start...");
     let p = embassy_nrf::init(Default::default());
+
+    let mut config = uarte::Config::default();
+    config.baudrate = uarte::Baudrate::BAUD115200;
+
+    let uart = uarte::Uarte::new(p.UARTE0, p.P0_08, p.P0_06, Irqs, config);
+    spawner.spawn(uart_task(uart)).unwrap();
 
     let temp = Temp::new(p.TEMP, Irqs);
 
@@ -59,6 +71,11 @@ async fn temp_task(mut temp: Temp<'static>) {
     loop {
         let value = temp.read().await.to_num::<u16>();
         info!("{} °C", value);
+        let mut s: String<64> = String::new();
+        if write!(s, ">Temp:{}\n", value).is_ok() {
+            UART_CHANNEL.send(s).await
+        }
+
         SIGNAL.signal(value);
         Timer::after_millis(INTERVAL_MS).await;
     }
@@ -96,5 +113,17 @@ async fn display_task(i2c: Twim<'static>) {
         } else if col == 0 {
             dir = 1;
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn uart_task(mut uart: uarte::Uarte<'static>) {
+    loop {
+        // La tâche attend qu'un message arrive dans le canal
+        let msg = UART_CHANNEL.receive().await;
+
+        // On cherche la fin réelle du message (au cas où il est plus court que 32 octets)
+        // ou on envoie le buffer fixe. Ici on envoie tout pour simplifier :
+        let _ = uart.write(msg.as_bytes()).await;
     }
 }
